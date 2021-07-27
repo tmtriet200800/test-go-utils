56c469ac06baee513f2fcf776087b8768721c902
Triet Tran <triettran200800@gmail.com>
2021-07-27 10:23:54 +0700

    update pkgApplication

--- application/doc.go
+++ application/doc.go
@@ -1,1 +1,7 @@
 package pkgApplication
+
+import "fmt"
+
+func Help(){
+	fmt.Println("pkgApplication handle neccessary application component")
+}
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+
+	pkgApplication "github.com/tmtriet200800/test-go-utils/application"
+)
+
+func main() {
+	fmt.Println("Welcome to our internal package")
+
+	pkgApplication.Help()
+}
